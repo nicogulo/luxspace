6187cf64af65dece0cb56840462a839f5c5e3b86
nicogulo <nicogulobelas@gmail.com>
2021-08-07 23:02:53 +0700

    setup tailwind 1 update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import 'assets/css/app.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-xl text-indigo-200">Hero</h1>
+      <h1 className="text-xl text-gray-900">Hero</h1>
     </div>
   );
 }
