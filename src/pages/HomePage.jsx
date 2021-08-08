ec8ffaa9df6ca89c5e22fb5ace0170080e84fa26
nicogulo <nicogulobelas@gmail.com>
2021-08-08 09:19:22 +0700

    section Browse Room

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from 'parts/Header';
 import Hero from 'parts/HomePage/Hero';
 import JustArrived from 'parts/HomePage/JustArrived';
+import BrowseRoom from 'parts/HomePage/BrowseRoom';
 
 export default function HomePage(props) {
   return (
     <>
       <Header />
       <Hero />
+      <BrowseRoom />
       <JustArrived />
     </>
   );
