f4f2f0d0f9e9a38988a46af06ce527bb650224f3
nicogulo <nicogulobelas@gmail.com>
2021-08-26 11:49:27 +0700

    add page congratulation and 404notfound

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from 'pages/HomePage';
 import Details from 'pages/Details';
 import Cart from 'pages/Cart';
+import Congratulation from 'pages/Congratulation';
+import NotFound from 'pages/NotFound';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route path="/categories/:idc" component={Details} />
         <Route path="/cart" component={Cart} />
+        <Route path="/congratulation" component={Congratulation} />
+        <Route path="*" component={NotFound} />
       </Router>
     </div>
   );
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from 'parts/Header';
+import Breadcrumb from 'components/Breadcrumb';
+
+import SiteMap from 'parts/SiteMap';
+import PageErrorMessage from 'parts/PageErrorMessage';
+import Footer from 'parts/Footer';
+
+export default function NotFound() {
+  return (
+    <>
+      <Header theme="black" />
+      <PageErrorMessage />
+      <SiteMap />
+      <Footer />
+    </>
+  );
+}
